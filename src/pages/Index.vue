11aff4a20aac920af3fdfd8a1d7a4105eed9928c
falcon <1769477451@qq.com>
2022-01-31 14:57:16 +0800

    1

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,6 +3,7 @@ import { isDark, toggleDark } from "~/composables/dark";
 </script>
 
 <template>
+    <div class="bg-light-100 hover:bg-dark-200">123123</div>
     <div m="6">Hello，This is the tov template！！</div>
     <div m="6" cursor="pointer" @click="toggleDark()">light: {{ isDark }} click me!!</div>
 </template>
